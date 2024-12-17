e160d51918d8cf548bc6e8ce7d05e1281dd77505
Alejandro <alejandro_gonzalo92@icloud.com>
2024-12-17 21:19:36 +0100

    app proper, servicio, repositorio, controlador
    se modifica appProperties
    
    se crea servicio
    
    se crea repositorio
    
    se crea controlador

---
+++ src/main/java/com/gestion/productos/controlador/ProductoControlador.java
@@ -0,0 +1,23 @@
+package com.gestion.productos.controlador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gestion.productos.modelo.Producto;
+import com.gestion.productos.servicio.ProductoServicio;
+
+@RestController
+public class ProductoControlador {
+    @Autowired
+    private ProductoServicio servicio;
+
+    @GetMapping("/productos")
+    public List<Producto> listarProductos(){
+        return servicio.listarProductos();
+    }
+
+
+}
