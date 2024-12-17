8d28d88f9e1d6f1db8ff13496810516433c17a40
Alejandro <alejandro_gonzalo92@icloud.com>
2024-12-17 21:41:16 +0100

    nuevo metodo
    se añade metodo obtener Producto en ProductoControlador.java

--- src/main/java/com/gestion/productos/controlador/ProductoControlador.java
+++ src/main/java/com/gestion/productos/controlador/ProductoControlador.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestion.productos.modelo.Producto;
@@ -17,6 +18,11 @@ public class ProductoControlador {
     @GetMapping("/productos")
     public List<Producto> listarProductos(){
         return servicio.listarProductos();
+    }
+
+    @GetMapping("/productos/{id}")
+    public Producto obteneProducto(@PathVariable Integer id){
+        return servicio.obtenerProductoPorId(id);
     }
 
 
